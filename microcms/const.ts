3133d2dcbd283ecdb67a4f42986a597eaf913be6
yutounun <geek.yuto@gmail.com>
2024-09-25 23:35:25 -0700

    customize tech stack section

--- microcms/const.ts
+++ microcms/const.ts
@@ -2,18 +2,6 @@ import { ITechStack } from "./type";
 
 export const TechStack: ITechStack[] = [
   {
-    language: "HTML",
-    imgUrl: "/images/html.png",
-  },
-  {
-    language: "CSS",
-    imgUrl: "/images/css.png",
-  },
-  {
-    language: "JavaScript",
-    imgUrl: "/images/javascript.png",
-  },
-  {
     language: "TypeScript",
     imgUrl: "/images/typescript.png",
   },
@@ -24,6 +12,42 @@ export const TechStack: ITechStack[] = [
   {
     language: "Next.js",
     imgUrl: "/images/nextjs.png",
+  },
+  {
+    language: "Vue.js",
+    imgUrl: "/images/html.png",
+  },
+  {
+    language: "FastAPI",
+    imgUrl: "/images/javascript.png",
+  },
+  {
+    language: "Node.js",
+    imgUrl: "/images/typescript.png",
+  },
+  {
+    language: "Django",
+    imgUrl: "/images/javascript.png",
+  },
+  {
+    language: "shadcn/ui",
+    imgUrl: "/images/javascript.png",
+  },
+  {
+    language: "tailwindcss",
+    imgUrl: "/images/javascript.png",
+  },
+  {
+    language: "Figma",
+    imgUrl: "/images/react.png",
+  },
+  {
+    language: "HeadlessCMS",
+    imgUrl: "/images/nextjs.png",
+  },
+  {
+    language: "AWS",
+    imgUrl: "/images/javascript.png",
   },
 ];
 
